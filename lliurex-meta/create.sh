14c42a3ff31d3185aafe509d10059a646b4f9444
Jose A. Múrcia <joamuran@gmail.com>
2017-01-16 11:49:01 +0100

    Added modifications for meta

--- lliurex-meta/create.sh
+++ lliurex-meta/create.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 
+METAPATH="./lliurex-meta/trunk/fuentes"
+
 # Clean lliurex-meta
 rm -r ./lliurex-meta
 # 1. Copy lliurex-meta from lliurex.net
 svn export https://svn.lliurex.net/xenial/lliurex-meta ./lliurex-meta
 
 # Add extra files for extra flavours
-cp -r ./llx-meta-hacks/* ./lliurex-meta/trunk/fuentes/
+cp -r ./llx-meta-hacks/* $METAPATH/
+
+
+
+## Modifying files ##
+
+# control
+cat ./expand-files/control >> $METAPATH/debian/control
+
+# STRUCTURE
+
+cat ./expand-files/STRUCTURE >> $METAPATH/seeds/lliurex/STRUCTURE
+
+#metapackage-map
+
+cat ./expand-files/metapackage-map  >> $METAPATH/
+
+
